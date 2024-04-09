470670b08c218e806ec97afdd21ef08947b398d2
alpharii <alphari404@gmail.com>
2024-04-10 00:13:40 +0700

    belajar use state

--- src/count.jsx
+++ src/count.jsx
@@ -7,13 +7,20 @@ function Count (){
     const [Count, setCount] = useState(0);
     const [Text, setText] = useState('Hello');
     const HandleText = () => {
-        setText(Text === 'Hello' ? 'World':'Hello');
+        setText(
+        Text === 'Hello' ? 'World':
+        Text === 'World' ? 'Saya':
+        Text === 'Saya' ? 'Muhammad':
+        Text === 'Muhammad' ? 'Bintang':
+        Text === 'Bintang' ? 'Alphari':
+        'Hello');
     }
 
     return(
         <div>
             <h1>counter = {Count}</h1>
             <button onClick={() => setCount(Count+1)}>Tambah</button>
+            <button onClick={() => setCount(Count-1)}>Kurang</button>
             <h1>{Text}</h1>
             <button onClick={HandleText}>Change Text</button>
         </div>
