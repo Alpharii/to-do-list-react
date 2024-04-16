a5cd22920d362567f459e1025c78fc5dc3d3d68e
alpharii <alphari404@gmail.com>
2024-04-16 18:27:53 +0700

    fungsionalitas to do list finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import UpdateArray from './belajar/UpdateArray.jsx'
+import Todolist from './todolist.jsx'
 import ListOfCar from './belajar/3-listOfCar.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ListOfCar/>
+    <Todolist />
   </React.StrictMode>,
 )
--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-function ToDoList(){
-    const addtask = () => {
+export const Todolist = () => {
+    const [task, setTask] = useState([])
 
+    const handleAddTaskButton = () => {
+        const newTask = document.getElementById('newTask').value
+        document.getElementById('newTask').value = '';
+        setTask(task => [...task, newTask])
     }
-    
+    const handleRemoveTask = (index) => {
+        setTask(task.filter((_,i) => i !== index))
+    }
 
-    return(
+  return (
     <div>
-        <h2>ToDo App</h2>
-        <input 
-        type="text"
-        placeholder='Masukan Kegiatanmu' />
-        <button onClick={addTask}><span>add +</span></button>
+        <h1>Todo List App</h1>
+        <input type="text" id='newTask' placeholder='Add a task' />
+        <button onClick={handleAddTaskButton}>Add Task</button>
+        <ul>
+            {task.map((task,index) => 
+            <div>
+                <li key={index}>{task}</li>
+                <button onClick={() => handleRemoveTask(index)}>Remove task</button>
+            </div>
+            )}
+        </ul>
     </div>
-    ) 
+  )
 }
 
-export default ToDoList
+export default Todolist
